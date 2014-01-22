2b1051223aa39bd0152a94493ba1fe23e96bb89b
Eduard Bondarenko <edbond@gmail.com>
2014-01-22 10:05:00 +0200

    extract xhrIo

--- src/cljs_http/core.cljs
+++ src/cljs_http/core.cljs
@@ -1,23 +1,36 @@
 (ns cljs-http.core
-  (:import goog.net.XhrIo)
+  (:import [goog.net XhrIo]
+           [goog.net EventType])
   (:require [cljs-http.util :as util]
-            [cljs.core.async :as async]))
+            [cljs.core.async :as async]
+            [goog.events :as gevents]))
+
 
 (defn request
   "Execute the HTTP request corresponding to the given Ring request
   map and return a core.async channel."
-  [{:keys [request-method headers body] :as request}]
+  [{:keys [request-method headers body abort with-credentials]
+    :or {:with-credentials true} :as request}]
   (let [channel (async/chan)
         method (name (or request-method :get))
         timeout (or (:timeout request) 0)
-        headers (util/build-headers headers)]
-    (XhrIo/send
-     (util/build-url request)
-     #(let [target (.-target %1)]
-        (->> {:status (.getStatus target)
-              :body (.getResponseText target)
-              :headers (util/parse-headers (.getAllResponseHeaders target))}
-             (async/put! channel))
-        (async/close! channel))
-     method body headers timeout true)
-    channel))
+        abort-chan (if abort (async/chan))
+        headers (util/build-headers headers)
+        xhr (XhrIo.)
+        cb #(let [target (.-target %1)]
+              (->> {:status (.getStatus target)
+                    :body (.getResponseText target)
+                    :headers (util/parse-headers (.getAllResponseHeaders target))}
+                   (async/put! channel))
+              (async/close! channel))]
+    (gevents/listen xhr EventType.COMPLETE cb)
+    (if with-credentials
+      (.setWithCredentials xhr with-credentials))
+    (if timeout
+      (.setTimeoutInterval xhr timeout))
+    (.send xhr (util/build-url request) method body headers)
+    (if abort
+      (do
+        (async/take! abort-chan #(.abort xhr))
+        [channel abort-chan])
+      channel)))
